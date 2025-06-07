1478a58d7eafc91714fe9c28e765f94bab90b55f
Andrey Kumykov <aakumykov@yandex.ru>
2025-06-07 21:39:05 +0300

    Исправления ошибок в LocalCloudWriter2.

--- local/src/main/java/com/github/aakumykov/local_cloud_writer/LocalCloudWriter2.kt
+++ local/src/main/java/com/github/aakumykov/local_cloud_writer/LocalCloudWriter2.kt
@@ -1,6 +1,5 @@
 package com.github.aakumykov.local_cloud_writer
 
-import com.github.aakumykov.cloud_writer.CloudWriter
 import com.github.aakumykov.cloud_writer.CloudWriter2
 import com.github.aakumykov.cloud_writer.CloudWriterException
 import java.io.File
@@ -22,19 +21,21 @@ class LocalCloudWriter2(
     }
 
     override fun createDeepDir(path: String, isRelative: Boolean): String {
-        return if (isRelative) createRelativeDeepDir(path)
-        else createAbsoluteDeepDir(path)
-    }
 
-    private fun createRelativeDeepDir(path: String): String {
-        return createAbsoluteDeepDir(virtualRootPlus(path))
-    }
+        // Так как идёт пошаговое создание
+        // каталогов из пути вглубь,
+        // то нужно отрезать от него "системную"
+        // незаписываемую часть.
 
-    private fun createAbsoluteDeepDir(path: String): String {
-        return iterateDeepOnPathWithAction(path) { partialPath ->
-            createAbsoluteDir(partialPath)
+        val pathToOperate = (if (isRelative) path
+        else path.replace(virtualRootPath, ""))
+            .replaceFirst(Regex("^/"),"")
+
+        return iterateDeepOnPathWithAction(pathToOperate) { partialPath ->
+            createRelativeDir(partialPath)
         }
     }
+
 
     /**
      * @return
@@ -43,10 +44,10 @@ class LocalCloudWriter2(
         return path
             .split(CloudWriter2.DS)
             .reduce { acc, s ->
-                createAbsoluteDir(acc)
+                action(acc)
                 acc + CloudWriter2.DS + s
             }.also { tailDir: String ->
-                createAbsoluteDir(tailDir)
+                action(tailDir)
             }
     }
 
@@ -89,13 +90,13 @@ class LocalCloudWriter2(
     }
 
     private fun fileExistsAbsolute(path: String): Boolean {
-        return File(virtualRootPlus(path)).exists()
+        return File(path).exists()
     }
 
 
     private fun virtualRootPlus(vararg pathParts: String): String {
         return mutableListOf(virtualRootPath).apply {
-            if (!addAll(0, pathParts.toList()))
+            if (!addAll(pathParts.toList()))
                 throw RuntimeException("Cannot add path parts to virtual root path.")
         }.joinToString(CloudWriter2.DS)
     }
