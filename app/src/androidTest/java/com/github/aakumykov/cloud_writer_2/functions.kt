5966357bb7137915e2582a03ffd61c948e15c335
Andrey Kumykov <aakumykov@yandex.ru>
2025-06-10 16:52:12 +0300

    Глобальное свойство shortId: String

--- app/src/androidTest/java/com/github/aakumykov/cloud_writer_2/functions.kt
+++ app/src/androidTest/java/com/github/aakumykov/cloud_writer_2/functions.kt
@@ -3,7 +3,13 @@ package com.github.aakumykov.cloud_writer_2
 import java.util.UUID
 
 val randomName: String
-    get() = randomId
+    get() = shortId
+
 
 val randomId: String
     get() = UUID.randomUUID().toString()
+
+
+val shortId: String
+    get() = randomId.split("-").first()
+
