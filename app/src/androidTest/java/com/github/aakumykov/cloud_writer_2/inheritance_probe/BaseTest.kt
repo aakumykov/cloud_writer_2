f658865b3e77115b577f00bc7bd62c8e108bbe5f
Andrey Kumykov <aakumykov@yandex.ru>
2025-06-16 11:41:06 +0300

    Наследование впервые получилось!

---
+++ app/src/androidTest/java/com/github/aakumykov/cloud_writer_2/inheritance_probe/BaseTest.kt
@@ -0,0 +1,31 @@
+package com.github.aakumykov.cloud_writer_2.inheritance_probe
+
+import com.github.aakumykov.cloud_writer.CloudWriter2
+import com.github.aakumykov.cloud_writer_2.common.StorageAccessTestCase
+import com.github.aakumykov.cloud_writer_2.common.randomName
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+
+abstract class BaseTest : StorageAccessTestCase() {
+
+    protected val dirName: String = randomName
+
+    protected abstract val cloudWriter2: CloudWriter2
+    protected abstract val virtualRootPath: String
+    protected abstract val absoluteDirPath: String
+
+
+    @Test
+    fun creates_local_dir() = run {
+        step("Создаю каталог '$dirName'") {
+            runTest {
+                cloudWriter2.createDir(dirName, true).also { createdDirPath ->
+                    step("Проверяю, что путь к нему соответствует '$absoluteDirPath'") {
+                        Assert.assertEquals(absoluteDirPath, createdDirPath)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/github/aakumykov/cloud_writer_2/inheritance_probe/LocalTest.kt
@@ -0,0 +1,23 @@
+package com.github.aakumykov.cloud_writer_2.inheritance_probe
+
+import android.os.Environment
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.aakumykov.cloud_writer.CloudWriter2
+import com.github.aakumykov.local_cloud_writer.LocalCloudWriter2
+import org.junit.runner.RunWith
+import java.io.File
+
+
+@RunWith(AndroidJUnit4::class)
+class LocalTest : BaseTest() {
+
+    override val virtualRootPath: String
+        get() = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath
+
+    override val cloudWriter2: CloudWriter2
+        get() = LocalCloudWriter2(virtualRootPath)
+
+    override val absoluteDirPath: String
+        get() = File(virtualRootPath, dirName).absolutePath
+
+}
