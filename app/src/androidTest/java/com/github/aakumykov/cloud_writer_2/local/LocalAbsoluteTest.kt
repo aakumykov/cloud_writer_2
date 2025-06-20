b89fb924754cc654d08a3b80eeaa594d03971d41
Andrey Kumykov <aakumykov@yandex.ru>
2025-06-20 12:24:41 +0300

    Revert "Начал разделять на части и запутался."
    This reverts commit 825b6cef206ebdc8e4a7f1cfe62d440d225a4524.

--- app/src/androidTest/java/com/github/aakumykov/cloud_writer_2/local/LocalAbsoluteTest.kt
+++ app/src/androidTest/java/com/github/aakumykov/cloud_writer_2/local/LocalAbsoluteTest.kt
@@ -1,7 +1,11 @@
 package com.github.aakumykov.cloud_writer_2.local
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
 
-class LocalAbsoluteBase : LocalBase(), Props {
+
+@RunWith(AndroidJUnit4::class)
+class LocalAbsoluteTest : LocalBase() {
 
     override val isRelative: Boolean = false
 
--- app/src/androidTest/java/com/github/aakumykov/cloud_writer_2/local/Props.kt
+++
@@ -1,8 +0,0 @@
-package com.github.aakumykov.cloud_writer_2.local
-
-interface Props {
-    val isRelative: Boolean
-    val dirPath: String
-    val deepDirPath: String
-    val filePath: String
-}
