73aa49b1508c5b70e76b4e0371a423149a25d63e
aakumykov <aakumykov@yandex.ru>
2025-08-04 17:48:05 +0300

    Начал писать VirtualRootPlusUnitTests

---
+++ cloud_writer/src/test/java/com/github/aakumykov/cloud_writer/VirtualRootPlusUnitTests.kt
@@ -0,0 +1,12 @@
+package com.github.aakumykov.cloud_writer
+
+import org.junit.Assert
+import org.junit.Test
+
+class VirtualRootPlusUnitTests {
+
+    @Test
+    fun `соединяет два пути в один`() {
+        Assert.assertEquals("","")
+    }
+}
