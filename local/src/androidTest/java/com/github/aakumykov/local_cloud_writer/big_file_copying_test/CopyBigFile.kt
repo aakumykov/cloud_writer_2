ed9ce4247fe48b18e2ca6862547d8cc0bbcabe40
Andrey Kumykov <aakumykov@yandex.ru>
2026-08-30 11:38:11 +0300

    Тест копирования большого файла.

---
+++ local/src/androidTest/java/com/github/aakumykov/local_cloud_writer/big_file_copying_test/CopyBigFile.kt
@@ -0,0 +1,49 @@
+package com.github.aakumykov.local_cloud_writer.big_file_copying_test
+
+import android.os.Environment
+import android.util.Log
+import com.github.aakumykov.local_cloud_writer.LocalCloudWriter
+import com.github.aakumykov.local_cloud_writer.currentTime
+import com.github.aakumykov.local_cloud_writer.kaspresso.StorageAccessTestCase
+import junit.framework.TestCase
+import org.junit.Assert
+import org.junit.Test
+import java.io.File
+
+class CopyBigFile : StorageAccessTestCase() {
+
+    private val localCloudWriter get() = LocalCloudWriter()
+
+    @Test
+    fun copying_big_file_duration() {
+
+        val sourceFileName = "debian.iso"
+        val targetFileName = "debian2.iso"
+
+        val downloadsDir =
+            File(Environment.getExternalStorageDirectory(), Environment.DIRECTORY_DOWNLOADS)
+        val musicDir =
+            File(Environment.getExternalStorageDirectory(), Environment.DIRECTORY_DOWNLOADS)
+
+        val sourceFile = File(downloadsDir, sourceFileName)
+        val targetFile = File(musicDir, targetFileName)
+
+        Assert.assertTrue(sourceFile.exists())
+
+        val startTime = currentTime
+        localCloudWriter.copyFile(
+            sourceFile.absolutePath,
+            targetFile.absolutePath,
+            overwriteIfExists = true
+        )
+        val duration = currentTime - startTime
+        Log.d(TAG,"продолжительность копирования $duration")
+
+        TestCase.assertTrue(sourceFile.exists())
+        TestCase.assertTrue(targetFile.exists())
+    }
+
+    companion object {
+        val TAG: String = CopyBigFile::class.java.simpleName
+    }
+}
